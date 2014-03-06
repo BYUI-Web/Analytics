3e500eb88cf5d310fb8a32cf024a574de742b480
Matthew Sessions <hyphrend@matthew.(none)>
2014-03-06 11:02:13 -0700

    Fixed bugs

--- s_code.js
+++ s_code.js
@@ -3,7 +3,7 @@ Copyright 1997-2006 Omniture, Inc. More info available at
 http://www.omniture.com */
 /* Specify the Report Suite ID(s) to track here */
 var s = new AppMeasurement();
-var s.account="byuibyuiedu";
+s.account="byuibyuiedu";
 
 /************************** CONFIG SECTION **************************/
 /* You may add or alter any code config here. */
